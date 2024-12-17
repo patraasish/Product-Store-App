06db12b17a214c2c36f0ff5cd5fe12202021f9f7
Asish Patra (OSV) <asish.patra.osv@fedex.com>
2024-12-17 15:25:45 +0530

    H2 DB setup done

---
+++ StoreApp/src/main/java/com/store/entity/Product.java
@@ -0,0 +1,58 @@
+package com.store.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name="product_details")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_brand")
+    private String brand;
+
+    @Column(name= "product_price")
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+
+}
